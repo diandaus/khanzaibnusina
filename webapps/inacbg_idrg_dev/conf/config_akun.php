2d69406fc2035a7dc72e991b62a30db8d12b781c
alfatih221121 <dianfirdaus73@gmail.com>
2025-10-29 13:42:20 +0700

    uploadskdp

---
+++ webapps/inacbg_idrg_dev/conf/config_akun.php
@@ -0,0 +1,23 @@
+<?php
+function getKey()
+{
+    $keyRS = "";
+    return $keyRS;
+}
+
+function getUrlWS()
+{
+    $UrlWS = "";
+    return $UrlWS;
+}
+
+function getKelasRS()
+{
+    $kelasRS = "CP";
+    return $kelasRS;
+}
+function getNIKCoder()
+{
+    $nik = "";
+    return $nik;
+}
